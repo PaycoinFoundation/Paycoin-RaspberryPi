7d7ca2ff7504a639fdaed4f083f6203dc35f4a7f
tvl83 <fxc4ygxcryBB>
2015-04-27 01:41:19 -0700

    Added a refresh button to the dashboard to get the most up to date data.
    This will later be automated into a timer to check every 5 or 10 seconds and any data that is different will glow or something to give an indication of what changed

--- public/scripts/controllers/DashboardCtrl.js
+++ public/scripts/controllers/DashboardCtrl.js
@@ -4,15 +4,20 @@ angular.module('PaycoinRpiWallet')
     .controller('DashboardCtrl', function ($scope, $rootScope, $http, paycoind) {
         $rootScope.app.curTitle = "Dashboard";
 
-        paycoind.getInfo()
-            .then(function(response){
-                $rootScope.getInfo = response;
-            }
-        );
+        $scope.refreshInfo = function() {
+
+            paycoind.getInfo()
+                .then(function (response) {
+                    $rootScope.getInfo = response;
+                }
+            )
+        };
 
         paycoind.listTransactions()
             .then(function(response){
                 $rootScope.listTransactions = response;
-            })
+            });
+
+        $scope.refreshInfo();
     }
 );
