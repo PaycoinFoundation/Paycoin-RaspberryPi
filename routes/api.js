b131d19a43ac1e095ad62f3ae117712f2fd7655e
tvl83 <fxc4ygxcryBB>
2015-04-28 23:29:14 -0700

    removed request module dependency and reference in the api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
 var fs = require('fs');
 var configJSON = require('../config/config.json');
 var addressesJSON = require('../data/addresses.json');
