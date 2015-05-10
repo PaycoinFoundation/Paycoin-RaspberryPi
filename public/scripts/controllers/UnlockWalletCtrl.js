a9f31deafdb84893c3041eef03e4d99127d0b62c
Thomas Le <tvle83@gmail.com>
2015-05-10 00:40:50 -0700

    Unlock wallet feature finished

--- public/scripts/controllers/UnlockWalletCtrl.js
+++ public/scripts/controllers/UnlockWalletCtrl.js
@@ -1,7 +1,22 @@
 'use strict';
 
 angular.module('PaycoinRpiWallet')
-    .controller('UnlockWalletCtrl', function ($scope, $rootScope) {
+    .controller('UnlockWalletCtrl', function ($scope, $rootScope, paycoind) {
         $rootScope.app.curTitle = "Unlock Wallet";
+
+        $scope.unlock = function() {
+            paycoind.unlock($scope.passphrase, $scope.duration)
+                .then(function (response) {
+                    console.log("unlock response");
+                    console.log(response);
+
+                    if(response.error){
+                        $scope.error = response.error.msg;
+                    } else {
+                        $scope.success = true;
+                    }
+
+                })
+        }
     }
 );
